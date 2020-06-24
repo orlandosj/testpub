0465f50e67775db4ef4ddf5bd8115c7b94cee8cc
orlandosj <orlandosj@gmail.com>
2020-06-24 07:30:27 -0300

    Criando componente

---
+++ src/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-input
+    v-model="value"
+    type="date"
+    placeholder=""
+    today-button
+    :label-today-button="Hoje"
+    close-button
+    :label-close-button="Fechar"
+    hide-header
+    :locale="en"
+  />
+</template>
+
+<script>
+export default {
+  name: 'MyComponent'
+}
+</script>
